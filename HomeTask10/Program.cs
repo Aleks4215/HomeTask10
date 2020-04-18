49c929e33fc3987a2735e6216114fd3f1e0e813d
Aleksey Evmenkov <aleksey4215@gmail.com>
2020-04-18 20:03:56 +0300

    v1

---
+++ HomeTask10/Program.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+
+namespace HomeTask10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+
+    public class Car
+    {
+        public double Volume { get; set; }
+        public string Vendor { get; set; }
+
+        public string FuelConsumption { get; set; }
+
+        public Car(double volume, string vendor, string fuelConsumption)
+        {
+            this.Volume = volume;
+            this.Vendor = vendor;
+            this.FuelConsumption = fuelConsumption;
+        }
+
+        public virtual double GetVolume(double volume)
+        {
+            return volume;
+        }
+
+        public static double operator -(Car a, Car b)
+        {
+            return a.Volume - b.Volume;
+        }
+        public static Car operator ++(Car car)
+        {
+            car.Volume++;
+            return car;
+        }
+
+
+    }
+    public class BMW : Car
+    {
+        public BMW(double volume, string vendor, string fuelConsumption) : base(volume, vendor, fuelConsumption)
+        {
+            this.Volume = volume;
+            this.Vendor = vendor;
+            this.FuelConsumption = fuelConsumption;
+        }
+    }
+}
